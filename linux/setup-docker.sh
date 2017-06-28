ff6e1b822844ac3b4443ae60123705207c0404c7
ezhu <ezhu@kubuntu.local>
2017-06-27 22:31:54 -0700

    Updating docker installation script

--- linux/setup-docker.sh
+++ linux/setup-docker.sh
@@ -9,7 +9,27 @@
 user=$1
 
 # install docker
-apt install -y docker.io
+apt install -yqq \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   edge"
+
+apt update
+
+apt install -yqq docker-ce
 
 # grant docker access to user
 gpasswd -a $user docker
